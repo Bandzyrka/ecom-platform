df7ec4145265482a92cefb4cce147286614c33bb
Bandzyrka <werstaptor@gmail.com>
2022-03-09 14:21:46 +0100

    adding navigation using react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const PlaceHoldPage = () =>(
+  <div>
+    <h1>
+      PlaceHoldPage
+    </h1>
+  </div>
+)
+
 function App() {
   return (
     <div> 
-      <HomePage /> 
+    <BrowserRouter>
+      <Switch> 
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/placeholder" component={PlaceHoldPage} /> 
+      </Switch>
+    </BrowserRouter>
     </div>
   );
 }
