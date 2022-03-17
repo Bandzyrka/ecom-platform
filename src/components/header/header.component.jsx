e04922bb5fda98722d8eb312719477754e184e4c
Bandzyrka <werstaptor@gmail.com>
2022-03-17 14:13:58 +0100

    Replacing scss with styled components on header and homepage

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,34 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import './header.styles.scss'
 import { ReactComponent as Logo } from '../../assets/086 crown.svg'
 import { auth } from '../../firebase/firebase.utils'
 import { connect } from 'react-redux'
 import CartIcon from '../shopping-icon/shopping-icon.compnent'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
+import {HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv} from './header.styles.jsx'
+
 const Header = ({currentUser, hidden}) => ( 
-    <div className="header">
-        <Link className="logo-container" to='/'> 
-            <Logo className="logo" /> 
-        </Link>
-        <div className="options">
-            <Link className="option" to='/shop'>SHOP</Link>
-            <Link className="option" to='/'>CONTACT</Link>
+    <HeaderContainer>
+        <LogoContainer to='/'> 
+            <Logo/> 
+        </LogoContainer>
+        
+        <OptionsContainer>
+            <OptionLink to='/shop'> SHOP </OptionLink>
+            <OptionLink to='/'> CONTACT </OptionLink>
             {
                 currentUser ?
-                <div className="option" onClick={() => auth.signOut()}> SIGN OUT </div> 
+                <OptionDiv onClick={() => auth.signOut()}> SIGN OUT </OptionDiv> 
                 :
-                <Link className="option" to='/signin'>SIGN IN</Link> 
+                <OptionLink to='/signin'> SIGN IN </OptionLink> 
             }
-        <CartIcon />
-        </div>
+            <CartIcon />
+        </OptionsContainer>
+            
         {
             hidden ?
             null
             : <CartDropdown />
         }
-    </div>
+    </HeaderContainer>
 )
 const mapStateToProps = ({user: { currentUser }, cart: {hidden}}) => ({
     currentUser,
---
+++ src/components/header/header.styles.jsx
@@ -0,0 +1,42 @@
+import styled, { css } from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const OptionsContainerStyles = css`
+  padding: 10px 15px;
+  text-decoration: underline 0.15em rgba(0, 0, 0, 0);
+  transition: text-decoration-color 300ms;
+  cursor: pointer;
+`
+export const HeaderContainer = styled.div`
+    height: 70px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 25px;
+    `
+  
+export const LogoContainer = styled(Link)`
+      height: 100%;
+      width: 70px;
+      padding: 25px;
+    `
+  
+export const OptionsContainer = styled.div`
+      width: 50%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+    `
+export const OptionLink = styled(Link)`
+  ${OptionsContainerStyles}
+  &:hover {
+    text-decoration-color: rgba(0, 0, 0, 1)
+  }
+`
+
+export const OptionDiv = styled.div`
+  ${OptionsContainerStyles}
+  &:hover {
+    text-decoration-color: rgba(0, 0, 0, 1)}
+`
