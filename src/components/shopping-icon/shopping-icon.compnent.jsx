76a237d5f3f79f60b546efc8c7a490d1187731d3
Bandzyrka <werstaptor@gmail.com>
2022-03-18 21:07:32 +0100

    updating shopping cart component with styled components

--- src/components/shopping-icon/shopping-icon.compnent.jsx
+++ src/components/shopping-icon/shopping-icon.compnent.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import './shopping-icon.styles.scss'
+
 import { ReactComponent as ShoppingIcon} from '../../assets/124 shopping-bag.svg'
 import { connect } from 'react-redux'
+
 import {toggleCartHidden} from '../../redux/cart/cart.actions'
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
+import {CartIconContainer, ShoppingIconContainer, ItemCounter } from './shopping-icon.styles'
+
 const CartIcon = ({toggleCartHidden, itemCount}) => (
-    <div onClick={toggleCartHidden} className="cart-icon">
-        <ShoppingIcon className="shopping-icon"/>
-        <span className="item-count">{itemCount}</span>
-    </div>
+    <CartIconContainer onClick={toggleCartHidden}>
+    <ShoppingIconContainer>
+        <ShoppingIcon/>
+    </ShoppingIconContainer>    
+    <ItemCounter>
+        {itemCount}
+    </ItemCounter>
+    </CartIconContainer>
 )
 
 const mapDispatchToProps = dispatch => ({
