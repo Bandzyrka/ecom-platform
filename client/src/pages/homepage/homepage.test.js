cafd42f083d4bfac7219872dab20ed7e0d7e4c8f
Bandzyrka <werstaptor@gmail.com>
2022-03-26 16:12:01 +0100

    Added enzyme, tests for HomePage

---
+++ client/src/pages/homepage/homepage.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import HomePage from './homepage.component';
+
+it('should render Homepage component', () => {
+  expect(shallow(<HomePage />)).toMatchSnapshot();
+});
