7fd64cec6e207fe6d5d571c9f1bec8d70a385ac0
Wiktor Bandzyra <werstaptor@gmail.com>
2022-04-12 13:18:41 +0200

    Updating slider with proper looping logic

--- client/src/components/image-slider/image-slider.component.jsx
+++ client/src/components/image-slider/image-slider.component.jsx
@@ -2,7 +2,7 @@ import React, { useReducer } from 'react'
 import { slidesData } from './slides-data'
 
 const initialState = {
-    slidesIndex: 0,
+    slideIndex: 0,
 }
 
 const slidesReducer = (state, action) => {
@@ -10,11 +10,14 @@ const slidesReducer = (state, action) => {
         case "NEXT_SLIDE":
             return{ 
                 ...state, 
-                slideIndex: state.slideIndex + 1 % slidesData.length}
+                slideIndex: (state.slideIndex + 1) % slidesData.length
+            }
         case "PREV_SLIDE":
             return{ 
                 ...state, 
-                slideIndex: state.slideIndex - 1 % slidesData.length
+                slideIndex: state.slideIndex === 0
+                ? slidesData.length - 1
+                : state.slideIndex - 1
             }
         default:
             return state;
@@ -25,6 +28,7 @@ const ImageSlider = () => {
   const [state, dispatch] = useReducer(slidesReducer, initialState)
   return (
     <div>
+        {state.slideIndex}
         {
             slidesData.map(slide => {
                 return(
@@ -34,8 +38,8 @@ const ImageSlider = () => {
                 )
             })
         }
-        <button onClick={() => dispatch("NEXT_SLIDE") }> NEXT </button>
-        <button onClick={() => dispatch("PREV_SLIDE") }> PREV </button>
+        <button onClick={() => dispatch({ type: "NEXT_SLIDE" })}> NEXT </button>
+        <button onClick={() => dispatch({ type: "PREV_SLIDE" })}> PREV </button>
     </div>
   )
 }
--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import Directory from "../../components/directory/directory.component"
 import { HomePageContainer } from './homepage.styles.jsx'
-
+import ImageSlider from '../../components/image-slider/image-slider.component'
 export const HomePage = () =>
     (
     <HomePageContainer>
-       
+        <ImageSlider />
         <Directory />
     </HomePageContainer>
     )
