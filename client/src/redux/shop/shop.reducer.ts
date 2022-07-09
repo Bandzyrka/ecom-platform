ced23bc27509206c6bc83d1cd248fbb330056518
Wiktor Bandzyra <werstaptor@gmail.com>
2022-07-09 13:11:31 +0200

    Replaced union pattern with matchable pattern in shop reducer

--- client/src/redux/shop/shop.reducer.ts
+++ client/src/redux/shop/shop.reducer.ts
@@ -1,46 +1,47 @@
-import { COLLECTIONS_ACTION_TYPES, Collection} from './shop.types';
-import { CollectionAction } from './shop.actions';
-
-
+import { Collection } from "./shop.types";
+import { AnyAction } from "redux";
+import {
+  fetchCollectionsStart,
+  fetchCollectionsSucces,
+  fetchCollectionsFailure,
+} from "./shop.actions";
 export type CollectionsState = {
   readonly collections: Collection[];
   readonly isCollectionFetching: boolean;
   readonly errorMessage: Error | null;
-}
-
+};
 
 export const COLLECTIONS_INITIAL_STATE: CollectionsState = {
   collections: [],
   isCollectionFetching: false,
-  errorMessage: null
+  errorMessage: null,
 };
 
 export const shopReducer = (
-    state = COLLECTIONS_INITIAL_STATE, 
-    action = {} as CollectionAction
-    ) => {
-
-    switch (action.type) {
-    case COLLECTIONS_ACTION_TYPES.FETCH_COLLECTIONS_START:
-      return {
-        ...state,
-        isCollectionFetching: true,
-      } 
-    case COLLECTIONS_ACTION_TYPES.FETCH_COLLECTIONS_SUCCES:
-      return {
-        ...state,
-        isCollectionFetching: false,
-        collections: action.payload
-      };
-    case COLLECTIONS_ACTION_TYPES.FETCH_COLLECTIONS_FAILURE:
-      return {
-        ...state,
-        isCollectionFetching: false,
-        errorMessage: action.payload
-      }
-    default:
-      return state;
+  state = COLLECTIONS_INITIAL_STATE,
+  action = {} as AnyAction
+) => {
+  if (fetchCollectionsStart.match(action)) {
+    return {
+      ...state,
+      isCollectionFetching: true,
+    };
   }
+  if (fetchCollectionsSucces.match(action)) {
+    return {
+      ...state,
+      isCollectionFetching: false,
+      collections: action.payload,
+    };
+  }
+  if (fetchCollectionsFailure.match(action)) {
+    return {
+      ...state,
+      isCollectionFetching: false,
+      errorMessage: action.payload,
+    };
+  }
+  return state;
 };
 
 export default shopReducer;
