2b26d2943b6ab255d8c5464178394a26f6f9808b
Wiktor Bandzyra <werstaptor@gmail.com>
2022-08-01 22:15:03 +0200

    Updated directory reducer with typescript

--- client/src/redux/directory/directory.selectors.ts
+++ client/src/redux/directory/directory.selectors.ts
@@ -1,6 +1,7 @@
 import { createSelector } from 'reselect'
+import { DirectoryState } from './directory.reducer';
 
-const selectDirectory = state => state.directory;
+const selectDirectory = (state): DirectoryState => state.directory;
 
 export const selectDirectorySections = createSelector(
     [selectDirectory],
